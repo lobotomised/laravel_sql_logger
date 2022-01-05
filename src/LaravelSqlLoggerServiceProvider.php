b0bd87b6f93c328739e036112a9340e9c0fa50d9
lobotomised <romain@rprevost.fr>
2022-01-05 20:47:24 +0100

    update key

--- src/LaravelSqlLoggerServiceProvider.php
+++ src/LaravelSqlLoggerServiceProvider.php
@@ -44,8 +44,7 @@ class LaravelSqlLoggerServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/sql-logger.php', 'db_debug'
+            __DIR__.'/../config/sql-logger.php', 'sql-logger'
         );
-
     }
 }
