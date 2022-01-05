0255b5c79c54d552f23f5fd7d8e92fee2d82f1ab
lobotomised <romain@rprevost.fr>
2022-01-05 20:23:36 +0100

    wip

--- src/LaravelSqlLoggerProvider.php
+++ src/LaravelSqlLoggerProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Support\ServiceProvider;
 use Lobotomised\LaravelSqlLogger\Commands\LaravelSqlLoggerCommand;
 use Illuminate\Support\Facades\DB;
 
-class LaravelSqlLogger extends ServiceProvider
+class LaravelSqlLoggerProvider extends ServiceProvider
 {
     public function boot()
     {
